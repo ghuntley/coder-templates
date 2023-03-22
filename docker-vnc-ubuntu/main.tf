bf02c87aa9adc1da2586f8472a1097314b2a3166
Geoffrey Huntley <ghuntley@ghuntley.com>
2023-03-22 01:21:29 +0000

    abc

--- docker-vnc-ubuntu/main.tf
+++ docker-vnc-ubuntu/main.tf
@@ -3,56 +3,94 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.4.2"
+      version = "~> 0.6.17"
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.16.0"
+      version = "~> 3.0.1"
     }
   }
 }
 
-provider "docker" {
-
+data "coder_provisioner" "me" {
 }
 
-provider "coder" {
+provider "docker" {
 }
 
 data "coder_workspace" "me" {
 }
 
-# Desktop
-resource "coder_app" "novnc" {
-  agent_id      = coder_agent.dev.id
-  name          = "noVNC Desktop"
-  icon          = "https://ppswi.us/noVNC/app/images/icons/novnc-192x192.png"
-  url           = "http://localhost:6080"
-  relative_path = true
+data "coder_parameter" "dotfiles_uri" {
+  name        = "What dotfiles repo would you like to use for your workspace?"
+  description = "Dotfiles repo URI (optional)"
+  default     = "https://github.com/ghuntley/dotfiles-coder"
+  type        = "string"
+  mutable     = true
 }
 
-# code-server
-resource "coder_app" "code-server" {
-  agent_id      = coder_agent.dev.id
-  name          = "code-server"
-  icon          = "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_vscode_icon_130084.png"
-  url           = "http://localhost:13337"
-  relative_path = true
-}
 
-resource "coder_agent" "dev" {
-  arch           = "amd64"
-  os             = "linux"
-  startup_script = <<EOT
-    #!/bin/bash
-    set -euo pipefail
+resource "coder_agent" "main" {
+  arch                   = data.coder_provisioner.me.arch
+  os                     = "linux"
+  env                    = { "DOTFILES_URI" = data.coder_parameter.dotfiles_uri.value != "" ? data.coder_parameter.dotfiles_uri.value : null }
+  login_before_ready     = false
+  startup_script_timeout = 180
+  startup_script         = <<-EOT
+    set -e
+    
+    # install and start code-server
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.8.3
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
 
-    # start code-server
-    code-server --auth none --port 13337 &
+    if [ -n "$DOTFILES_URI" ]; then
+      echo "Installing dotfiles from $DOTFILES_URI"
+      coder dotfiles "$DOTFILES_URI" --yes
+    fi
 
-    echo "Initializing Supervisor..."
-    nohup supervisord
   EOT
+}
+
+resource "coder_app" "novnc" {
+  agent_id     = coder_agent.main.id
+  slug         = "novnc"
+  display_name = "noVNC"
+  url          = "http://localhost:80/"
+  icon         = ""
+  subdomain    = false
+  share        = "owner"
+}
+
+resource "coder_app" "code-server" {
+  agent_id     = coder_agent.main.id
+  slug         = "code-server"
+  display_name = "code-server"
+  url          = "http://localhost:13337/?folder=/home/ghuntley"
+  icon         = "/icon/code.svg"
+  subdomain    = false
+  share        = "owner"
+
+  healthcheck {
+    url       = "http://localhost:13337/healthz"
+    interval  = 3
+    threshold = 10
+  }
+}
+
+data "coder_parameter" "container_enable_dind" {
+  name        = "Enable Docker in Docker in Docker?"
+  description = "This is insecure"
+  default     = "false"
+  type        = "bool"
+  mutable     = true
+  option {
+    name  = "false"
+    value = false
+  }
+  option {
+    name  = "true"
+    value = true
+  }
 }
 
 resource "docker_volume" "home_volume" {
@@ -82,8 +120,36 @@ resource "docker_volume" "home_volume" {
   }
 }
 
+resource "docker_volume" "nix_volume" {
+  name = "coder-${data.coder_workspace.me.id}-nix"
+  # Protect the volume from being deleted due to changes in attributes.
+  lifecycle {
+    ignore_changes = all
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace.me.owner
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace.me.owner_id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  # This field becomes outdated if the workspace is renamed but can
+  # be useful for debugging or cleaning out dangling volumes.
+  labels {
+    label = "coder.workspace_name_at_creation"
+    value = data.coder_workspace.me.name
+  }
+}
+
+
 data "docker_registry_image" "coder_image" {
-  name = "fredblgr/ubuntu-novnc:20.04"
+  name = "ubuntu:precise"
 }
 
 resource "docker_image" "coder_image" {
@@ -95,24 +161,66 @@ resource "docker_image" "coder_image" {
 }
 
 resource "docker_container" "workspace" {
+
+  # Use the Sysbox container runtime
+  # runtime = "sysbox-runc"
+
+  # enable docker-in-docker-in-docker nb: reduces security 
+  privileged = data.coder_parameter.container_enable_dind.value
+  mounts {
+    source = data.coder_parameter.container_enable_dind.value ? "/var/run/docker.sock" : "/dev/null"
+    target = "/var/run/docker.sock"
+    type = "bind"
+  }
+
   count = data.coder_workspace.me.start_count
-  image = docker_image.coder_image.latest
-  
+  image = data.docker_registry_image.coder_image.name
   # Uses lower() to avoid Docker restriction on container names.
   name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
   # Hostname makes the shell more user friendly: coder@my-workspace:~$
-  hostname = lower(data.coder_workspace.me.name)
-  dns      = ["1.1.1.1"]
-  # Use the docker gateway if the access URL is 127.0.0.1 
-  command = ["sh", "-c", replace(coder_agent.dev.init_script, "127.0.0.1", "host.docker.internal")]
-  env     = ["CODER_AGENT_TOKEN=${coder_agent.dev.token}"]
+  hostname = data.coder_workspace.me.name
+  # Use the docker gateway if the access URL is 127.0.0.1
+  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
+  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
   host {
     host = "host.docker.internal"
     ip   = "host-gateway"
   }
   volumes {
-    container_path = "/home/coder/"
+    container_path = "/home/ghuntley/"
     volume_name    = docker_volume.home_volume.name
     read_only      = false
+  }
+  volumes {
+    container_path = "/nix/"
+    volume_name    = docker_volume.nix_volume.name
+    read_only      = false
+  }
+  # Add labels in Docker to keep track of orphan resources.
+  labels {
+    label = "coder.owner"
+    value = data.coder_workspace.me.owner
+  }
+  labels {
+    label = "coder.owner_id"
+    value = data.coder_workspace.me.owner_id
+  }
+  labels {
+    label = "coder.workspace_id"
+    value = data.coder_workspace.me.id
+  }
+  labels {
+    label = "coder.workspace_name"
+    value = data.coder_workspace.me.name
+  }
+}
+
+resource "coder_metadata" "container_info" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = docker_container.workspace[0].id
+
+  item {
+    key   = "image"
+    value = data.docker_registry_image.coder_image.name
   }
 }
